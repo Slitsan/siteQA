ae767fb0a2e9f4d1ecae7050901a8bded73f1c22
rtDima <dima@rt-ed.com>
2022-02-28 13:39:40 +0200

    initial

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+# 28_2_2022
+# created by Dima
+#  POM automation app for rt-ed.co.il
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    return 'hi ' + str(name)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(print_hi('PyCharm'))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
