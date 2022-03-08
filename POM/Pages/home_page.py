87a89b50c2e492f400f10b207d577d155b694c93
Pablik <pablikmanshirov1998@gmail.com>
2022-03-08 11:14:40 +0200

    finished with automation for registration in home page

--- POM/Pages/home_page.py
+++ POM/Pages/home_page.py
@@ -19,6 +19,27 @@ class HomePage:
         self.maslul_real_time_in_form_popup_by_xpath = "//*[@id='rt-leads-form-1'] /descendant::select/option[2]"
         self.terms_of_agreement_field_in_form_popup_by_xpath = "//*[@id='rt-leads-form-1'] /descendant::input[5]"
         self.send_button_in_form_popup_by_xpath = "//*[@id='rt-leads-form-1'] /descendant::button"
+        # -------------------------------------------MAIN FORM-------------------------------------------------------
+        self.last_name_field_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::input[2]"
+        self.first_name_field_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::input[1]"
+        self.phone_number_field_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::input[4]"
+        self.email_field_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::input[3]"
+        self.terms_of_agreement_field_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::input[5]"
+        self.choose_maslul_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::select"
+        self.maslul_real_time_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::select/option[2]"
+        self.send_button_in_main_form_by_xpath = "//*[@id='rt-leads-form-2'] /descendant::button"
+        # -----------------------------------------FLOATING FORM---------------------------------------------------
+        self.floating_menu_by_xpath = "//*[@id=\"floating-form\"]"
+        self.about_us_in_floating_menu_by_xpath = "//*[@id='floating-form'] /descendant::div[2]"
+        self.form_in_floating_menu_by_xpath = "//*[@id='floating-form'] /descendant::div[3]"
+        self.last_name_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::input[@id='lname']"
+        self.first_name_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::input[@id='fname']"
+        self.phone_number_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::input[@id='phone']"
+        self.email_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::input[@id='email']"
+        self.choose_maslul_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::select"
+        self.choose_maslul_real_time_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::option[2]"
+        self.terms_of_agreement_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::input[@class='px-2']"
+        self.send_button_in_floating_form_by_xpath = "//*[@id=\"lead-form-modal\"] /descendant::button"
 
     # --------------------------------POPUP METHODS---------------------------------------------------
     def popup_form(self):
@@ -50,3 +71,63 @@ class HomePage:
 
     def send_button_in_form_popup(self):
         return self.driver.find_element(By.XPATH, self.send_button_in_form_popup_by_xpath)
+
+    # -----------------------------------------MAIN FORM METHODS------------------------------------------
+    def last_name_field_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.last_name_field_in_main_form_by_xpath)
+
+    def first_name_field_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.first_name_field_in_main_form_by_xpath)
+
+    def phone_number_field_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.phone_number_field_in_main_form_by_xpath)
+
+    def email_field_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.email_field_in_main_form_by_xpath)
+
+    def choose_maslul_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.choose_maslul_in_main_form_by_xpath)
+
+    def choose_maslul_real_time_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.choose_maslul_real_time_in_main_form())
+
+    def terms_of_agreement_field_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.terms_of_agreement_field_in_main_form_by_xpath)
+
+    def send_button_in_main_form(self):
+        return self.driver.find_element(By.XPATH, self.send_button_in_main_form_by_xpath)
+
+    # --------------------------------------FLOATING FORM METHODS-----------------------------------------
+
+    def floating_menu(self):
+        return self.driver.find_element(By.XPATH, self.floating_menu_by_xpath)
+
+    def about_us_in_floating_menu(self):
+        return self.driver.find_element(By.XPATH, self.about_us_in_floating_menu_by_xpath)
+
+    def form_in_floating_menu(self):
+        return self.driver.find_element(By.XPATH, self.form_in_floating_menu_by_xpath)
+
+    def last_name_field_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.last_name_in_floating_form_by_xpath)
+
+    def first_name_field_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.first_name_in_floating_form_by_xpath)
+
+    def phone_number_field_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.phone_number_in_floating_form_by_xpath)
+
+    def email_field_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.email_in_floating_form_by_xpath)
+
+    def choose_maslul_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.choose_maslul_in_floating_form_by_xpath)
+
+    def choose_maslul_real_time_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.choose_maslul_real_time_in_floating_form_by_xpath)
+
+    def terms_of_agreement_field_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.terms_of_agreement_in_floating_form_by_xpath)
+
+    def send_button_in_floating_form(self):
+        return self.driver.find_element(By.XPATH, self.send_button_in_floating_form_by_xpath)
--- POM/Tests/home_page_test.py
+++ POM/Tests/home_page_test.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime
 import unittest
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from POM.Pages.home_page import HomePage
@@ -31,6 +32,7 @@ class HomePageTest(unittest.TestCase):
         cls.test_user_mail = current_time + "@test.com"
         cls.test_user_phone_number = current_time
 
+    # ---------------------------------POPUP TEST-----------------------------------------
     def send_keys_to_last_name_field_in_popup(self):
         print("---Inside send_keys_to_last_name_field_in_popup function---")
         home_page = HomePage(self.driver)
@@ -81,15 +83,149 @@ class HomePageTest(unittest.TestCase):
         home_page.close_button_in_popup_form().click()
         print("---Outside wait_for_popup function---")
 
-    def test_popup_form(self):
-        # self.send_keys_to_last_name_field_in_popup()
-        # self.send_keys_to_first_name_field_in_popup()
-        # self.send_keys_to_mail_field_in_popup()
-        # self.send_keys_to_phone_number_field_in_popup()
-        # self.choose_maslul_real_time_in_popup()
-        # self.tick_terms_and_services_button_in_popup()
-        # self.click_on_send_button_in_popup()
-        self.close_popup()
+    # def test_popup_form(self):
+    # self.send_keys_to_last_name_field_in_popup()
+    # self.send_keys_to_first_name_field_in_popup()
+    # self.send_keys_to_mail_field_in_popup()
+    # self.send_keys_to_phone_number_field_in_popup()
+    # self.choose_maslul_real_time_in_popup()
+    # self.tick_terms_and_services_button_in_popup()
+    # self.click_on_send_button_in_popup()
+    # self.close_popup()
+
+    # -----------------------------------------MAIN FORM TEST---------------------------------------
+    def send_keys_to_last_name_field_in_main_form(self):
+        print("---Inside send_keys_to_last_name_field_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.last_name_field_in_main_form().send_keys(self.test_user_last_name)
+        print("---Outside send_keys_to_last_name_field_in_main_form function---")
+
+    def send_keys_to_first_name_field_in_main_form(self):
+        print("---Inside send_keys_to_first_name_field_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.first_name_field_in_main_form().send_keys(self.test_user_first_name)
+        print("---Outside send_keys_to_first_name_field_in_main_form function---")
+
+    def send_keys_to_mail_field_in_main_form(self):
+        print("---Inside send_keys_to_mail_field_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.email_field_in_main_form().send_keys(self.test_user_mail)
+        print("---Outside send_keys_to_mail_field_in_main_form function---")
+
+    def send_keys_to_phone_number_field_in_main_form(self):
+        print("---Inside send_keys_to_phone_number_field_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.phone_number_field_in_main_form().send_keys(self.test_user_phone_number)
+        print("---Outside send_keys_to_phone_number_field_in_main_form function---")
+
+    def choose_maslul_real_time_in_main_form(self):
+        print("---Inside choose_maslul_real_time_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.choose_maslul_in_main_form().click()
+        home_page.choose_maslul_real_time_in_main_form().click()
+        print("---Outside choose_maslul_real_time_in_main_form function---")
+
+    def tick_terms_and_services_button_in_main_form(self):
+        print("---Inside tick_terms_and_services_button_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.terms_of_agreement_field_in_main_form().click()
+        print("---Outside tick_terms_and_services_button_in_main_form function---")
+
+    def click_on_send_button_in_main_form(self):
+        print("---Inside click_on_send_button_in_main_form function---")
+        home_page = HomePage(self.driver)
+        home_page.send_button_in_main_form().click()
+        print("---Outside click_on_send_button_in_main_form function---")
+
+    # def test_main_form(self):
+    #     self.close_popup()
+    #     self.send_keys_to_last_name_field_in_main_form()
+    #     self.send_keys_to_first_name_field_in_main_form()
+    #     self.send_keys_to_mail_field_in_main_form()
+    #     self.send_keys_to_phone_number_field_in_main_form()
+    #     self.choose_maslul_real_time_in_main_form()
+    #     self.tick_terms_and_services_button_in_main_form()
+    #     self.click_on_send_button_in_main_form()
+    # -----------------------------------FLOATING FORM TEST-------------------------------
+
+    def hover_on_floating_menu(self):
+        print("---Inside click_on_floating_menu function...---")
+        home_page = HomePage(self.driver)
+        actions = ActionChains(self.driver)
+        actions.move_to_element(home_page.floating_menu()).perform()
+        # home_page.floating_menu().click()
+        print("---Outside click_on_floating_menu function...---")
+
+    def click_on_form_in_floating_menu(self):
+        print("---Inside click_on_form_in_floating_menu function...---")
+        home_page = HomePage(self.driver)
+        home_page.form_in_floating_menu().click()
+        print("---Outside click_on_form_in_floating_menu function...---")
+
+    def send_keys_to_last_name_field_in_floating_form(self):
+        print("---Inside send_keys_to_last_name_field_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.last_name_field_in_floating_form().send_keys(self.test_user_last_name)
+        print("---Outside send_keys_to_last_name_field_in_floating_form function---")
+
+    def send_keys_to_first_name_field_in_floating_form(self):
+        print("---Inside send_keys_to_first_name_field_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.first_name_field_in_floating_form().send_keys(self.test_user_first_name)
+        print("---Outside send_keys_to_first_name_field_in_floating_form function---")
+
+    def send_keys_to_mail_field_in_floating_form(self):
+        print("---Inside send_keys_to_mail_field_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.email_field_in_floating_form().send_keys(self.test_user_mail)
+        print("---Outside send_keys_to_mail_field_in_floating_form function---")
+
+    def send_keys_to_phone_number_field_in_floating_form(self):
+        print("---Inside send_keys_to_phone_number_field_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.phone_number_field_in_floating_form().send_keys(self.test_user_phone_number)
+        print("---Outside send_keys_to_phone_number_field_in_floating_form function---")
+
+    def choose_maslul_real_time_in_floating_form(self):
+        print("---Inside choose_maslul_real_time_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.choose_maslul_in_floating_form().click()
+        home_page.choose_maslul_real_time_in_floating_form().click()
+        print("---Outside choose_maslul_real_time_in_floating_form function---")
+
+    def tick_terms_and_services_button_in_floating_form(self):
+        print("---Inside tick_terms_and_services_button_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.terms_of_agreement_field_in_floating_form().click()
+        print("---Outside tick_terms_and_services_button_in_floating_form function---")
+
+    def click_on_send_button_in_floating_form(self):
+        print("---Inside click_on_send_button_in_floating_form function---")
+        home_page = HomePage(self.driver)
+        home_page.send_button_in_floating_form().click()
+        print("---Outside click_on_send_button_in_floating_form function---")
+
+    # def test_floating_menu(self):
+    #     self.hover_on_floating_menu()
+    #     self.click_on_form_in_floating_menu()
+    #     self.send_keys_to_last_name_field_in_floating_form()
+    #     self.send_keys_to_first_name_field_in_floating_form()
+    #     self.send_keys_to_mail_field_in_floating_form()
+    #     self.send_keys_to_phone_number_field_in_floating_form()
+    #     self.choose_maslul_real_time_in_floating_form()
+    #     self.tick_terms_and_services_button_in_floating_form()
+        #self.click_on_send_button_in_floating_form()
+    # -----------------------------------FLOATING ABOUT US TEST----------------------------
+
+    def click_on_about_us_in_floating_menu(self):
+        print("---Inside click_on_about_us_in_floating_menu function...---")
+        home_page = HomePage(self.driver)
+        home_page.about_us_in_floating_menu().click()
+        print("---Outside click_on_about_us_in_floating_menu function...---")
+
+    # def test_floating_about_us(self):
+    #     self.hover_on_floating_menu()
+    #     self.click_on_about_us_in_floating_menu()
 
 
 if __name__ == '__main__':
